596f0bd75bd4fd3d1f51d55bb5f1b4fe6c3b7a28
dmertl <dmertl@gmail.com>
2014-10-04 00:10:46 -0700

    Removing unused method.

--- scraper/util.py
+++ scraper/util.py
@@ -1,28 +1,6 @@
 import os
 import urllib2
 
-#TOOD: remove
-def scrape_to_dict(menu_scrape):
-    """
-    Convert a MenuScrape to dict that can be easily dumped to JSON.
-
-    :param menu_scrape:
-    :type menu_scrape: MenuScrape
-    :return:
-    :rtype: dict
-    """
-    d = {
-        'location': menu_scrape.location.__dict__.copy(),
-        'url': menu_scrape.url,
-        'date': menu_scrape.date.isoformat() if menu_scrape.date else None,
-        'beverages': []
-    }
-    for beverage in menu_scrape.beverages:
-        d['beverages'].append(
-            dict((k, v) for k, v in beverage.__dict__.iteritems() if v)
-        )
-    return d
-
 
 def flatten_beverages(beverages):
     """
