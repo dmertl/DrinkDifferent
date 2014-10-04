721153b7d5be51954bfc28b62e0ffa2f7140ac56
dmertl <dmertl@gmail.com>
2014-10-04 00:06:10 -0700

    Adding method for standard scrape arg handling. Checks location name, filesystem, else HTTP URL.

--- scraper/util.py
+++ scraper/util.py
@@ -1,5 +1,5 @@
-__author__ = 'dmertl'
-
+import os
+import urllib2
 
 def scrape_to_dict(menu_scrape):
     """
@@ -38,3 +38,27 @@ def flatten_beverages(beverages):
             dict((k, v) for k, v in beverage.__dict__.iteritems() if v)
         )
     return flat
+
+
+def url_from_arg(arg, locations):
+    """
+    Determine actual URL from scrape request argument.
+
+    Checks if argument is location name or file on local filesystem. Otherwise assumes HTTP URL. Standard process for
+    running scraper from the command line.
+
+    :param arg:
+    :type arg: str
+    :param locations:
+    :type locations: Location[]
+    :return:
+    :rtype: str
+    """
+    location = [x for x in locations if x.name == arg]
+    if location:
+        return location[0].url
+    else:
+        if os.path.exists(arg):
+            return 'file:{0}'.format(urllib2.quote(os.path.abspath(arg)))
+        else:
+            return arg
